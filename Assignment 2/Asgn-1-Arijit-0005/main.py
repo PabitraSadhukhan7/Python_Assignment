9ea8d53e1640d81a5d95adbe96ef7d1fcbeefa69
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2026-05-16 11:07:36 +0000

    Assignment 2 done by Arijit Chowdhury

---
+++ Assignment 2/Asgn-1-Arijit-0005/main.py
@@ -0,0 +1,20 @@
+class Unique:
+    def __init__(self):
+        self.hash = {}
+    
+    def remove_duplicates(self, list):
+        for i in list:
+            if i not in self.hash:
+                self.hash[i] = 1
+            self.hash[i] += 1
+        return [key for key in self.hash.keys()]
+
+l = list(
+    map(
+        lambda x: int(x), 
+        input("Enter the list: ").split(" ")
+    )
+)
+
+u = Unique()
+print(u.remove_duplicates(l))
